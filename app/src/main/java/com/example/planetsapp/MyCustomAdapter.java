62b7a51c1e15f142bddfcddca1d3d0de00020736
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-31 22:10:50 +0530

    Created a Java file 'MyCustomAdapter.java' and added a Custom Adapters with View Holder Classes.

--- app/src/main/java/com/example/planetsapp/MyCustomAdapter.java
+++ app/src/main/java/com/example/planetsapp/MyCustomAdapter.java
@@ -1,9 +1,14 @@
 package com.example.planetsapp;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
@@ -15,5 +20,19 @@ public class MyCustomAdapter extends ArrayAdapter<Planet> {
         super(context, R.layout.item_list_layout, planetArrayList);
         this.planetArrayList = planetArrayList;
         this.context = context1;
+    }
+
+    private static class MyViewHolder {
+        TextView planetName;
+        TextView planetMoonCount;
+        ImageView planetImage;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Planet planet = getItem(position);
+        MyViewHolder myViewHolder;
+        final View result;
     }
 }
